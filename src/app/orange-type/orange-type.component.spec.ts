988f4f2bd625f33aaeca1869f7b090c6133b14da
De Leon Saucedo <luz.de.leon.saucedo@accenture.com>
2018-06-10 02:31:59 -0500

    Styling -globals, variables, general layout

--- src/app/orange-type/orange-type.component.spec.ts
+++ src/app/orange-type/orange-type.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { OrangeTypeComponent } from './orange-type.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('OrangeTypeComponent', () => {
+  let component: OrangeTypeComponent;
+  let fixture: ComponentFixture<OrangeTypeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ OrangeTypeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(OrangeTypeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
